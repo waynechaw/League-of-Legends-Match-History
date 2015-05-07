e974bdcde7d77b0c3f4766d1706235c36958615c
waynechaw <wayne.chaw@gmail.com>
2015-05-06 23:00:26 -0700

    port

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());
-var https = require('https');
-
-app.use(express.static(__dirname + '/public'));
-
-app.post('/riot', function(request, response) {
-
-  var name = request.body.summonerName;
-
-	var body = "";
-	var profile;
-
-	var riotAPI = https.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name + "?api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb", function(riotResponse){
-
-
-		riotResponse.on('data', function(chunk){
-			body += chunk;
-			//console.log(body);
-			profile = body;
-
-			response.type('json');
-  			response.end(profile);
-
-		});
-
-	});
-
-});
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
