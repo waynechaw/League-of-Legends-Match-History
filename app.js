e8455708ceaa7b0b3277c82166fae39adbbc5c0d
waynechaw <wayne.chaw@gmail.com>
2015-05-06 23:13:45 -0700

    port

--- app.js
+++ app.js
@@ -2,26 +2,22 @@
 
 var express = require('express');
 var app = express();
-/*
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 var https = require('https');
 
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-*/
 
 
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
 
 
 /*
