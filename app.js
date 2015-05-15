83c47342df3b515b3e0773f223a016c15c9bd176
waynechaw <wayne.chaw@gmail.com>
2015-05-14 20:48:07 -0700

    "add match history function"

--- app.js
+++ app.js
@@ -27,28 +27,59 @@ app.post('/riot', function(request, response) {
 
   var name = request.body.summonerName;
 
-
-
 	var riotAPI = https.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name + "?api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb", function(riotResponse){
-
 		var body = "";
-		
 		riotResponse.on('data', function(chunk){
-
 			body+= chunk;
-
 		});
-
 		riotResponse.on('end', function(){
-
 			response.type('json');
-			response.end(body);
-			
-		});
-	
+			response.end(body);		
+		});	
+	});
+
+});
+
+app.post('/match', function(request, response) {
+
+  var id = request.body.summonerID;
+
+  var url = "https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/" + id + "?api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb"
 
 
+	var riotAPI = https.get(url, function(riotResponse){
+		var body = "";
+		riotResponse.on('data', function(chunk){
+			body+= chunk;
 		});
+		riotResponse.on('end', function(){
+			response.type('json');
+			//console.log(body);
+			response.end(body);		
+		});	
+	});
+
+});
+
+
+app.post('/champ', function(request, response) {
+
+  var champID = request.body.champID;
+
+  var url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + champID + "?api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb"
+
+
+	var riotAPI = https.get(url, function(riotResponse){
+		var body = "";
+		riotResponse.on('data', function(chunk){
+			body+= chunk;
+		});
+		riotResponse.on('end', function(){
+			response.type('json');
+			//console.log(body);
+			response.end(body);		
+		});	
+	});
 
 });
 
---
+++ public/script.js
@@ -0,0 +1,111 @@
+$('#btn').click(function() {
+
+  $('.historyContainer').html("");
+
+      var SUMMONER_NAME = "";
+      SUMMONER_NAME = $("#userName").val().replace(/ /g, "").toLowerCase();
+
+      if (SUMMONER_NAME !== "") {
+
+        var data = {};
+        var summonerID;
+        data.summonerName = SUMMONER_NAME;
+
+
+          $.ajax({
+              url: '/riot',
+              type: 'POST',
+              contentType: 'application/json',
+              data: JSON.stringify(data),
+              success: function(data) {
+
+                  var summonerLevel = data[SUMMONER_NAME].summonerLevel;
+                  summonerID = data[SUMMONER_NAME].id;
+
+                  $("#sLevel").css("color", "black"); 
+                  $("#sLevel").html(summonerLevel);
+
+                  matchHistory(summonerID);
+                },
+              error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+                  $("#sLevel").html("summoner name not found");
+                  $("#sLevel").css("color", "red"); 
+              }
+          });
+
+      } else {
+
+          $("#sLevel").html("Please enter summoner name");
+          $("#sLevel").css("color", "red"); 
+
+      }
+});
+
+var convertResult = function(str){
+  if (str == "FALSE"){
+    return "loss"
+  }else{
+    return "win"
+  }
+}
+
+var prependMatch = function(champName, result, role, lane) {
+  var newMatch = '<span class="bold">Champion:</span> ' + champName +  ' | <span class="bold">Result: </span>' + convertResult(result) + ' | <span class="bold">Role:</span> ' + role.toLowerCase() + ' | <span class="bold">Lane: </span>' + lane.toLowerCase() + '<br><hr>';
+
+  $('.historyContainer').prepend(newMatch);
+}
+
+var addMatch = function(champName, result, role, lane){
+  //alert(champName + " " + result + " " + role + " " + lane);
+
+  var data = {};
+  data.champID = champName;
+
+
+
+  $.ajax({
+    url: '/champ',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function(data) {
+
+      var champName = data.name;
+      
+      prependMatch(champName, result, role, lane)
+
+    }
+
+  });
+
+
+}
+
+
+
+var matchHistory = function(summonerID){
+
+  var data = {};
+  data.summonerID = summonerID;
+
+  $.ajax({
+    url: '/match',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    success: function(data) {
+
+      
+      data.matches.forEach(function(match){
+        addMatch(match.participants[0].championId, match.participants[0].stats.winner, match.participants[0].timeline.role, match.participants[0].timeline.lane);
+      });
+
+      
+
+
+    }
+
+  });
+}
+
