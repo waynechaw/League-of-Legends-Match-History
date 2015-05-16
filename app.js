6b3cd25f8cc2e338dabdd126d014efdce6f1058f
waynechaw <wayne.chaw@gmail.com>
2015-05-16 00:01:46 -0700

    cleaned up code

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-
-
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
 var https = require('https');
-
-var pg = require('pg');
-
 var favicon = require('serve-favicon');
 
-
-
-app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -30,7 +22,7 @@ app.post('/riot', function(request, response) {
 	var riotAPI = https.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name + "?api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb", function(riotResponse){
 		var body = "";
 		riotResponse.on('data', function(chunk){
-			body+= chunk;
+			body += chunk;
 		});
 		riotResponse.on('end', function(){
 			response.type('json');
@@ -46,44 +38,39 @@ app.post('/match', function(request, response) {
 
   var url = "https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/" + id + "?api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb"
 
-
-
 	var riotAPI = https.get(url, function(riotResponse){
 		var body = "";
 		riotResponse.on('data', function(chunk){
-			body+= chunk;
+			body += chunk;
 		});
 		riotResponse.on('end', function(){
 			response.type('json');
-			
 			response.end(body);		
 		});	
 	});
 
 });
-
-
 
 app.post('/map', function(request, response) {
 
-
   var url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?dataById=true&api_key=3cfc7c5c-611d-4fc6-9154-7801d822e7cb"
-
 
 	var riotAPI = https.get(url, function(riotResponse){
 		var body = "";
 		riotResponse.on('data', function(chunk){
-			body+= chunk;
+			body += chunk;
 		});
+
 		riotResponse.on('end', function(){
 			response.type('json');
-			//console.log(body);
 			response.end(body);		
 		});	
 	});
 
 });
 
+/*
+var pg = require('pg');
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
@@ -95,3 +82,4 @@ app.get('/db', function (request, response) {
     });
   });
 })
+*/
--- public/script.js
+++ public/script.js
@@ -1,48 +1,29 @@
-
 var animateLoading = function(){
   var load = setInterval(function () {
-
     if ($('#status').html() == "Loading..."){
       $('#status').html("Loading")
     }else{
       $('#status').append(".");
     }
 
-
-
     if ($('.historyContainer').html() != ""){
       clearInterval(load);
       $('#status').html("");
     }
-
-
-
-
  }, 400);
 }
 
-
-
-
 $('#btn').click(function() {
-
   $('.historyContainer').html("");
-
-
-
-
       var SUMMONER_NAME = "";
       SUMMONER_NAME = $("#userName").val().replace(/ /g, "").toLowerCase();
-
       if (SUMMONER_NAME !== "") {
 
         $('#status').html("Loading");
         animateLoading();
 
         var data = {};
-        var summonerID;
         data.summonerName = SUMMONER_NAME;
-
 
           $.ajax({
               url: '/riot',
@@ -50,42 +31,30 @@ $('#btn').click(function() {
               contentType: 'application/json',
               data: JSON.stringify(data),
               success: function(data) {
-
                   var summonerLevel = data[SUMMONER_NAME].summonerLevel;
-                  summonerID = data[SUMMONER_NAME].id;
-
+                  var summonerID = data[SUMMONER_NAME].id;
                   $("#sLevel").css("color", "black"); 
                   $("#sLevel").html(summonerLevel);
-
                   matchHistory(summonerID);
                 },
               error: function (XMLHttpRequest, textStatus, errorThrown) {
-
                   $("#sLevel").html("summoner name not found");
                   $("#sLevel").css("color", "red"); 
               }
           });
-
       } else {
-
           $("#sLevel").html("Please enter summoner name");
           $("#sLevel").css("color", "red"); 
-
       }
 });
 
 var convertResult = function(str){
-
   if (str == false){
     return "loss"
   }else{
     return "win"
   }
 }
-
-
-
-
 
 var matchHistory = function(summonerID){
 
@@ -99,7 +68,6 @@ var matchHistory = function(summonerID){
     data: JSON.stringify(data),
     success: function(data) {
 
-
       var matchArray = [];
 
       data.matches.forEach(function(match){
@@ -112,44 +80,23 @@ var matchHistory = function(summonerID){
           time: match.matchCreation
         })
       });
-
       transformTime(matchArray);
-
-
-
-      
-
-
     }
-
   });
 }
 
 var transformTime = function(arr){
    
-
-
-
     $.ajax({
     url: '/map',
     type: 'POST',
     contentType: 'application/json',
     success: function(data) {
 
-      //console.log (data);
-      //console.log (arr);
-
-
       arr.forEach(function(match){
         match.champ = data.data[match.champ].name;
-        match.time = match.time;
       });
-
-
-
-      updateMatch(arr.sort(function(b, a){return a.time - b.time}));
-
-
+      updateMatch(arr);
     }
   });
 
@@ -157,24 +104,9 @@ var transformTime = function(arr){
 
 var updateMatch = function(arr) {
 
-  console.log(arr);
-
   arr.forEach(function(match){
-
-
-    var newMatch = 
-    {
-      html: '<span class="block"><span class="bold">Champion:</span> ' + match.champ +  '</span> | <span class="block2"><span class="bold">Result: </span>' + convertResult(match.result) + '</span> | <span class="block2"><span class="bold">Role:</span> ' + match.role.toLowerCase().replace("_", " ") + '</span> | <span class="bold">Lane: </span>' + match.lane.toLowerCase() + '<br><hr>',
-      time: match.time
-    }
-
-    $('.historyContainer').append(newMatch.html);
-
-
-
+    var newMatch = '<span class="block"><span class="bold">Champion:</span> ' + match.champ +  '</span> | <span class="block2"><span class="bold">Result: </span>' + convertResult(match.result) + '</span> | <span class="block2"><span class="bold">Role:</span> ' + match.role.toLowerCase().replace("_", " ") + '</span> | <span class="bold">Lane: </span>' + match.lane.toLowerCase() + '<br><hr>';
+    $('.historyContainer').prepend(newMatch);
   });
-
-
-
 
 }
